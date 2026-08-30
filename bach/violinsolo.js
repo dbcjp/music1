7f134959eb4e05ad19bae967d6a6d2607ff85f99
dbcjp <gtalkoct@gmail.com>
2026-08-31 01:57:02 +0900

    Update violinsolo.js

--- bach/violinsolo.js
+++ bach/violinsolo.js
@@ -594,3 +594,9 @@ addData(
 "Amandine Beyer",
 "Beyer, Amandine"
 );
+addData(
+"8NDo2a9vsLs,OCd-b1thqxM,YBE_iWzr2Gc,7USh70y2I7s,T46guU2MPMU,ewa0ooKIp80,6VcAszwD-VE,Mm2Oa1KuvpY,_qA2TsUDz0o,cIj3WpPmTWw,8a88zFN-MM8,hizGqEHXBHs,R2KbmPYopc0,Wj2Ym55lGg8,WOd1TjGdWn4,OQcPyG91xEM,CgDO0jTkSEI,-45n45mZKyg,u5wK6-ya83M,xZCRCGpp2j0,ncXbZKElucI,PIYb0Fif2es,rKYdFMgQHxA,ti48yVzC1HM,Wkjas9_1EWE,zXjHRDtc_Fo,G-Rh1tFBjK0,suqbK-Q_6jw,DZQXkEsjX8M,jLq4Q97a6e4,mRitHJFGTec",
+"*Sonata No. 1 in G Minor|1. Adagio,@bwv1001-1/2. Fuga,@bwv1001-2/3. Siciliano,@bwv1001-3/4. Presto,@bwv1001-4/*Partita No. 1 in B Minor|1. Allemande,@bwv1002-1/2. Double,@bwv1002-2/3. Corrente,@bwv1002-3/4. Double,@bwv1002-4/5. Sarabanda,@bwv1002-5/6. Double,@bwv1002-6/7. Tempo di Bourree,@bwv1002-7/8. Double,@bwv1002-8/*Sonata No. 2 in A Minor|1. Grave,@bwv1003-1/2. Fuga,@bwv1003-2/3. Andante,@bwv1003-3/4. Allegro,@bwv1003-4/*Partita No. 2 in D Minor|1. Allemande,@bwv1004-1/2. Corrente,@bwv1004-2/3. Sarabande,@bwv1004-3/4. Giga,@bwv1004-4/5. Ciaccona,@bwv1004-5/*Sonata No. 3 in C Major|1. Adagio,@bwv1005-1/2. Fuga,@bwv1005-2/3. Largo,@bwv1005-3/4. Allegro assai,@bwv1005-4/*Partita No. 3 in E Major|1. Preludio,@bwv1006-1/2. Loure,@bwv1006-2/3. Gavotte en rondeau,@bwv1006-3/4. Menuet I II,@bwv1006-4/6. Bourree,@bwv1006-6/7. Gigue,@bwv1006-7",
+"Susanne Lautenbacher",
+"Lautenbacher, Susanne"
+);
