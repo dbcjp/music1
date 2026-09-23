9f9fa66e92be8fbf6c3247c38df414c0f8d34e81
dbcjp <gtalkoct@gmail.com>
2026-09-24 00:38:34 +0900

    Create lutz.js

---
+++ bach/cantata/lutz.js
@@ -0,0 +1,6 @@
+addData(
+" -yxUuGaPPsc,pzaC_2iT61Y,_IiL16aEEsU,_Qmz8Xhmt9o,vpySZF274jM,TPgebf-dzHw,HPv73zfBfKM,-SKijBXrgcU,UpS_Q9xwf88,MA5MsKm2B8A",
+"*Herz und Mund und Tat und Leben|(Pt.1) BWV147-1,@BWV147-1/(Pt.1) BWV147-2,@BWV147-2/(Pt.1) BWV147-3,@BWV147-3/(Pt.1) BWV147-4,@BWV147-4/(Pt.1) BWV147-5,@BWV147-5/(Pt.1) BWV147-6,@BWV147-6/(Pt.2) BWV147-7,@BWV147-7/(Pt.2) BWV147-8,@BWV147-8/(Pt.2) BWV147-9,@BWV147-9/(Pt.2) BWV147-10,@BWV147-10",
+"Rudolf Lutz · Chor der J.S. Bach-Stiftung · Orchester der J.S. Bach-Stiftung",
+"Lutz, Rudolf · Chor der J.S. Bach-Stiftung · Orchester der J.S. Bach-Stiftung"
+);
